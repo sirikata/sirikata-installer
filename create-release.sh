d382c54d5bb98ae27d4a07affa5dd6b6ed513010
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-05-31 13:53:03 -0700

    Add update support.
    Build InstallBuilder autoupdater binaries, install as an optional
    component including an update.ini to control it, and add a Start Menu
    entry to access it. Also generate an update.xml file to go in
    sirikata.com/releases, which is polled to check for updates.
    
    We also need to generate an extra version of the mac installer because
    the autoupdater needs a .tgz instead of the .dmg we were already
    producing.

--- create-release.sh
+++ create-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# create-release.sh x.y.z
+# create-release.sh x y z
 #
 # Grab the raw binaries and make the necessary modifications to create
 # installers for each platform. Essentially, this downloads and
@@ -10,11 +10,18 @@
 # PATH, in which case it will be run automatically to build the
 # packages.
 
-VERSION=$1
+VERSION_MAJOR=$1
+VERSION_MINOR=$2
+VERSION_REVISION=$3
+
+VERSION=${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_REVISION}
+VERSION_INT=$((${VERSION_MAJOR}*1000000 + ${VERSION_MINOR}*1000 + ${VERSION_REVISION}))
 
 # Clean out any old data
 rm -rf sirikata_win32
 rm -rf sirikata_mac
+rm -f sirikata-win32-autoupdate.exe
+rm -rf sirikata-mac-autoupdate.app
 rm -rf sirikata-${VERSION}-mac-installer.app
 rm -f sirikata-${VERSION}-mac-installer.dmg
 rm -f sirikata-${VERSION}-win32-installer.exe
@@ -22,10 +29,10 @@ rm -f sirikata-${VERSION}-win32-installer.exe
 # Grab and extract data
 echo "Grabbing and extracting raw packages..."
 ([ -e sirikata-$VERSION-win32.zip ] || wget http://sirikata.com/releases/win32/sirikata-$VERSION-win32.zip) && \
-    unzip sirikata-$VERSION-win32.zip
+    unzip -q sirikata-$VERSION-win32.zip
 
 ([ -e sirikata-$VERSION-mac.tar.gz ] || wget http://sirikata.com/releases/mac/sirikata-$VERSION-mac.tar.gz) && \
-    tar -xzvf sirikata-$VERSION-mac.tar.gz
+    tar -xzf sirikata-$VERSION-mac.tar.gz
 
 # Make sure we have a copy of sirikata at the right version. We get
 # the README and LICENSE from here currently.
@@ -39,22 +46,58 @@ echo "Grabbing and extracting raw packages..."
 # support not having it in your PATH, we just generate a copy with the
 # version set. Using a copy avoids accidentally checking in changes to
 # this file.
-sed "s/<version><\/version>/<version>${VERSION}<\/version>/" sirikata.xml > sirikata-$VERSION.xml
-
+sed "s/__VERSIONINT__/${VERSION_INT}/" sirikata.xml | sed "s/__VERSION__/${VERSION}/" > sirikata-$VERSION.xml
+# And generate update.xml with correct version info for the server
+sed "s/__VERSIONINT__/${VERSION_INT}/" update-template.xml | sed "s/__VERSION__/${VERSION}/" > update.xml
 
 # Try to run InstallBuilder if we can find it
 INSTALLBUILDER=`which builder`
 if [ "x${INSTALLBUILDER}" == "x" ]; then
     echo "Couldn't find InstallBuilder. You need to open sirikata-${VERSION}.xml manually and build the packages."
 else
-    echo "Building packages for Windows and Mac..."
+    INSTALLBUILDER_BIN=`dirname ${INSTALLBUILDER}`
+    INSTALLBUILDER_BASE=`dirname ${INSTALLBUILDER_BIN}`
+    INSTALLBUILDER_AUTOUPDATE=${INSTALLBUILDER_BASE}/autoupdate/bin/customize.run
+
+    echo "Building autoupdaters"
+    ${INSTALLBUILDER_AUTOUPDATE} build sirikata-autoupdate.xml windows
+    ${INSTALLBUILDER_AUTOUPDATE} build sirikata-autoupdate.xml osx
+    echo "Copying to current directory..."
+    cp -r ${INSTALLBUILDER_BASE}/autoupdate/output/autoupdate-windows.exe sirikata-win32-autoupdate.exe
+    cp -r ${INSTALLBUILDER_BASE}/autoupdate/output/autoupdate-osx.app sirikata-mac-autoupdate.app
+
+
+    echo "Building installers for Windows and Mac..."
     ${INSTALLBUILDER} build sirikata-$VERSION.xml windows
     ${INSTALLBUILDER} build sirikata-$VERSION.xml osx
     echo "Copying to current directory..."
+    # Copy installer app, works for both regular and update
     cp -r `dirname ${INSTALLBUILDER}`/../output/Sirikata-${VERSION}-osx-installer.app sirikata-${VERSION}-mac-installer.app
+    # Generate dmg for regular installs
     genisoimage -D -V "sirikata-${VERSION}" -no-pad -r -apple -root sirikata-${VERSION}-mac-installer.app -o sirikata-${VERSION}-mac-installer.dmg sirikata-${VERSION}-mac-installer.app
+    # Regular archive for updater
+    tar -czf sirikata-${VERSION}-mac-installer.tgz sirikata-${VERSION}-mac-installer.app
+    # Copy the windows installer
     cp -r `dirname ${INSTALLBUILDER}`/../output/Sirikata-${VERSION}-windows-installer.exe sirikata-${VERSION}-win32-installer.exe
     echo "Done"
 
+
     rm sirikata-${VERSION}.xml
+
+    echo
+    echo
+    echo "*****************************"
+    echo
+    echo "Finished generating installers. You need to upload:"
+    echo " * the installer packages to sirikata.com/releases/[mac/win32]"
+    echo " * the update installer package (tgz version) for mac to sirikata.com/releases/mac"
+    echo " * update.xml to sirikata.com/releases/"
+    echo
+    echo "   e.g."
+    echo "    scp sirikata-${VERSION}-win32-installer.exe user@server:sirikata.com/releases/win32/"
+    echo "    scp sirikata-${VERSION}-mac-installer.dmg user@server:sirikata.com/releases/mac/"
+    echo "    scp sirikata-${VERSION}-mac-installer.dmg user@server:sirikata.com/releases/mac/"
+    echo "    scp update.xml user@server:sirikata.com/releases/"
+    echo
+
 fi
